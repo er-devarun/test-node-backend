9063d4c1c98e9cd605e43a45743fe878d8265c44
Arun Dev <er.devarun@gmail.com>
2024-08-01 21:55:30 +0530

    update file

--- data/items.js
+++ data/items.js
@@ -1,15 +1,20 @@
 const fs = require('node:fs/promises');
+const path = require('path');
+
+// Define paths relative to the current file
+const itemsFilePath = path.join(__dirname, 'items.json');
+const itemsDataFilePath = path.join(__dirname, 'itemsData.json');
 
 async function getStoredItems() {
-  const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
+  const rawFileContent = await fs.readFile(itemsFilePath, { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
   return storedItems;
 }
 
 async function getItems() {
-  const rawFileContent = await fs.readFile('itemsData.json', { encoding: 'utf-8' });
-  const data = await JSON.parse(rawFileContent);
+  const rawFileContent = await fs.readFile(itemsDataFilePath, { encoding: 'utf-8' });
+  const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
   return storedItems;
 }
